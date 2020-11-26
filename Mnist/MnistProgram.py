f481eca0c39f1ce72a3f7dcb3e24095804c139ae
fred460f <frederik@rovi.dk>
2020-11-26 12:21:37 +0100

    pog

--- Mnist/MnistProgram.py
+++ Mnist/MnistProgram.py
@@ -38,47 +38,47 @@ test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=1)
 print("Restored model, accuracy: {:5.3f}%".format(100 * test_acc))
 
 
-predictions = model.predict(test_images)
-# Modellens gæt på første stykke tøj.
-print("Modellens Gæt:", np.argmax(predictions[0]))
-# Hvad første stykke tøj faktisk var.
-print("Korrekte stykke tøj:", test_labels[0])
+# predictions = model.predict(test_images)
+# # Modellens gæt på første stykke tøj.
+# print("Modellens Gæt:", np.argmax(predictions[0]))
+# # Hvad første stykke tøj faktisk var.
+# print("Korrekte stykke tøj:", test_labels[0])
 
 
-# COLOR = 'white'
-# plt.rcParams['text.color'] = COLOR
-# plt.rcParams['axes.labelcolor'] = COLOR
+COLOR = 'black'
+plt.rcParams['text.color'] = COLOR
+plt.rcParams['axes.labelcolor'] = COLOR
 
-# def predict(model, image, correct_label):
-#   class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
-#                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-#   prediction = model.predict(np.array([image]))
-#   predicted_class = class_names[np.argmax(prediction)]
+def predict(model, image, correct_label):
+  class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+  prediction = model.predict(np.array([image]))
+  predicted_class = class_names[np.argmax(prediction)]
 
-#   show_image(image, class_names[correct_label], predicted_class)
-
-
-# def show_image(img, label, guess):
-#   plt.figure()
-#   plt.imshow(img, cmap=plt.cm.binary)
-#   plt.title("Excpected: " + label)
-#   plt.xlabel("Guess: " + guess)
-#   plt.colorbar()
-#   plt.grid(False)
-#   plt.show()
+  show_image(image, class_names[correct_label], predicted_class)
 
 
-# def get_number():
-#   while True:
-#     num = input("Pick a number: ")
-#     if num.isdigit():
-#       num = int(num)
-#       if 0 <= num <= 1000:
-#         return int(num)
-#     else:
-#       print("Try again...")
+def show_image(img, correct_label, guess):
+  plt.figure()
+  plt.imshow(img, cmap=plt.cm.get_cmap('binary'))
+  plt.title("Programmets gæt: " + guess, fontsize=18)
+  plt.xlabel("Objektet er: " + correct_label, fontsize=18)
+  plt.colorbar()
+  plt.grid(False)
+  plt.show()
 
-# num = get_number()
-# image = test_images[num]
-# label = test_labels[num]
-# predict(model, image, label)
+
+def get_number():
+  while True:
+    num = input("Vælg et tal: ")
+    if num.isdigit():
+      num = int(num)
+      if 0 <= num <= 1000:
+        return int(num)
+    else:
+      print("Prøv igen...")
+
+num = get_number()
+image = test_images[num]
+label = test_labels[num]
+predict(model, image, label)
